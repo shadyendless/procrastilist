addb46ba0f0f0b0b74b16c446cec2304f7238176
Jacob Foster <dalindeck@gmail.com>
2016-06-27 16:54:11 -0700

    Can this file disappear?

--- resources/views/index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Procrastilist</title>
-  <link href="/build/style.62643ecfb0aeac5eb677.css" rel="stylesheet"></head>
-  <body>
-  <script type="text/javascript" src="/build/manifest.348cefea23cdcb1de415.js"></script><script type="text/javascript" src="/build/vendor.8a88cfb2d9b600a6862c.js"></script><script type="text/javascript" src="/build/style.62643ecfb0aeac5eb677.js"></script><script type="text/javascript" src="/build/app.ce88ca55e57588144f4a.js"></script></body>
-</html>
